ece338eed84093243e9821dc8e1af55f5b8d5636
Luis Jimenez <luisjc140992@gmail.com>
2025-07-04 20:12:54 -0600

    animated pulse square

---
+++ pages/01a_your-first-animation/script.js
@@ -0,0 +1,20 @@
+gsap.to(".card", {
+  opacity: 1,
+  duration: 2,
+  ease: "power2.inOut",
+  onComplete: () => {
+    gsap.to(".card", {
+      y: -20,
+      repeat: -1,
+      yoyo: true,
+      duration: 0.5,
+    });
+
+    gsap.to(".card", {
+        boxShadow: "0 20px 80px rgba(29, 209, 161, 0.6)",
+        repeat: -1,
+        yoyo: true,
+        duration: 1,
+      });
+  },
+})

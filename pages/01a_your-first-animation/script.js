a6c594c9e1f558c4441e3d54676cdb8f9f6a5cc5
Luis Jimenez <luisjc140992@gmail.com>
2025-07-04 21:46:00 -0600

    add GSAP dependency and implement card animation in challenge

--- pages/01a_your-first-animation/script.js
+++ pages/01a_your-first-animation/script.js
@@ -9,12 +9,5 @@ gsap.to(".card", {
       yoyo: true,
       duration: 0.5,
     });
-
-    gsap.to(".card", {
-        boxShadow: "0 20px 80px rgba(29, 209, 161, 0.6)",
-        repeat: -1,
-        yoyo: true,
-        duration: 1,
-      });
   },
-})
+});
